f37cea75e7485fde427401481ba06d434b25b94c
AnikDasPartha58 <Anikdaspartha58@gmail.com>
2022-09-12 14:14:57 +0600

    edit

---
+++ js/words.js
@@ -0,0 +1,12 @@
+let words = [
+    {
+        word: "addition",
+        hint: "The process of adding numbers"
+    },
+    {
+        word: "meeting",
+        hint: "Event in which people come together"
+    },
+
+   
+]
